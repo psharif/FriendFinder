f83ff8f6fdf642438b473e04efd53f6bfebc5580
psharif <psharif@asu.edu>
2018-02-26 22:25:11 -0800

    Added Files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,27 @@
 module.exports = [
 	{
 		name: "Foxy Unicorn",
-		photoLink: "http://www.cucumberpress.com/wp-content/uploads/2017/07/Draw-Image-Of-Unicorn-84-On-To-Download-with-Image-Of-Unicorn.jpg",
+		photoLink: "https://media.giphy.com/media/26AHG5KGFxSkUWw1i/giphy.gif",
 		survey: ["3", "4", "2", "2", "4", "5", "3", "4", "5", "2"]
 	},
 	{
 		name: "Penguin Prow",
-		photoLink: "https://en.wiktionary.org/wiki/penguin#/media/File:SGI-2016-South_Georgia_(Fortuna_Bay)%E2%80%93King_penguin_(Aptenodytes_patagonicus)_04.jpg",
+		photoLink: "https://media.giphy.com/media/H4uE6w9G1uK4M/giphy.gif",
 		survey: ["4", "3", "5", "2", "5", "3", "3", "2", "1", "4"]
+	},
+	{
+		name: "Skillet Crayons",
+		photoLink: "https://media.giphy.com/media/ZOBxxM0E7cnzq/giphy.gif",
+		survey: ["3", "1", "2", "2", "4", "4", "4", "4", "4", "2"]
+	},
+	{
+		name: "Carmel Onions",
+		photoLink: "https://media.giphy.com/media/6UunYWYyexF7i/giphy.gif",
+		survey: ["1", "5", "5", "5", "5", "1", "5", "2", "1", "1"]
+	},
+	{
+		name: "Krippy Snowman",
+		photoLink: "https://media.giphy.com/media/mbTRlJ2AwBZMk/giphy.gif",
+		survey: ["5", "2", "1", "1", "3", "2", "1", "1", "1", "4"]
 	}
 ];
