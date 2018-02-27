ce88c9172e7aef041563e62cc6d9bb975bf67c89
psharif <psharif@asu.edu>
2018-02-26 17:11:10 -0800

    Added Necessary styling for Image

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,11 +2,11 @@ module.exports = [
 	{
 		name: "Foxy Unicorn",
 		photoLink: "http://www.cucumberpress.com/wp-content/uploads/2017/07/Draw-Image-Of-Unicorn-84-On-To-Download-with-Image-Of-Unicorn.jpg",
-		'survey[]': ["3", "4", "2", "2", "4", "5", "3", "4", "5", "2"]
+		survey: ["3", "4", "2", "2", "4", "5", "3", "4", "5", "2"]
 	},
 	{
 		name: "Penguin Prow",
 		photoLink: "https://en.wiktionary.org/wiki/penguin#/media/File:SGI-2016-South_Georgia_(Fortuna_Bay)%E2%80%93King_penguin_(Aptenodytes_patagonicus)_04.jpg",
-		'survey[]': ["4", "3", "5", "2", "5", "3", "3", "2", "1", "4"]
+		survey: ["4", "3", "5", "2", "5", "3", "3", "2", "1", "4"]
 	}
 ];
